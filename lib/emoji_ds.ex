9ca39ca20a5ad182751f33439f24e6ad24ca031f
eltonplima <eltonplima@gmail.com>
2019-05-27 19:47:38 -0300

    Finish the first version EmojiFinder.DataSet
    Signed-off-by: eltonplima <eltonplima@gmail.com>

--- lib/emoji_ds.ex
+++ lib/emoji_ds.ex
@@ -1,11 +1,20 @@
 defmodule EmojiFinder.DataSet do
+  defp extract_fields(line) do
+    [emoji_unicode, words | _rest] = line |> String.split(";", trim: true)
+    [emoji_unicode, words]
+  end
+
+  defp split_words([emoji_unicode, words]) do
+    splited_words = String.split(words) |> MapSet.new()
+      %{emoji_unicode => splited_words}
+  end
+
   def load(emojis_file) do
-    [emoji_unicode, words | _rest] = File.open!(emojis_file)
-    |> IO.read(:line)
-    |> String.trim("\n")
-    |> String.split(";")
-    words = String.split(words) |> MapSet.new()
-    %{emoji_unicode => words}
+    File.read!(emojis_file)
+    |> String.split("\n", trim: true)
+    |> Enum.map(&extract_fields(&1))
+    |> Enum.map(&split_words(&1))
+    |> Enum.reduce(&Map.merge(&1, &2))
     #%{"UTF-CHAR" => MapSet( )}
     # Conjunto de emoticons vinculados a uma palavra
     # emojis=%{"cat" => MapSet.new(["xpto", "xpta"]), "eye" => MapSet.new(["xpte", "xpto"]), "hand" => MapSet.new(["xptb", "xptc"]), "foot" => MapSet.new(["xptd"])}
--- test/emoji_ds_test.exs
+++ test/emoji_ds_test.exs
@@ -4,12 +4,19 @@ defmodule EmojiFinder.DataSetTest do
   doctest EmojiFinder.DataSet
 
   test "test load data" do
-    content = "1F481;INFORMATION DESK PERSON;So;0;ON;;;;;N;;;;;\n"
-    words = String.split("INFORMATION DESK PERSON")
-    expected = %{"1F481" => MapSet.new(words)}
-
+    content = """
+    1F481;INFORMATION DESK PERSON;So;0;ON;;;;;N;;;;;
+    1F639;CAT FACE WITH TEARS OF JOY;So;0;ON;;;;;N;;;;;
+    """
     {:ok, fd, emojis_file} = Temp.open("test.txt")
     IO.write(fd, content)
+
+    words_unicode_1 = String.split("INFORMATION DESK PERSON")
+    words_unicode_2 = String.split("CAT FACE WITH TEARS OF JOY")
+    expected = %{
+      "1F481" => MapSet.new(words_unicode_1),
+      "1F639" => MapSet.new(words_unicode_2)
+    }
 
     assert EmojiFinder.DataSet.load(emojis_file) == expected
   end
